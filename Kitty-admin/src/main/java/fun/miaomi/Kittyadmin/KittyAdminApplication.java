515c1170ea025b8a9dc6c4890888ee60f30c3c2a
shenxianweilai <1981502077@qq.com>
2022-08-24 09:44:17 +0800

    Add files via upload

---
+++ Kitty-admin/src/main/java/fun/miaomi/Kittyadmin/KittyAdminApplication.java
@@ -0,0 +1,13 @@
+package fun.miaomi.Kittyadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittyAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KittyAdminApplication.class, args);
+	}
+
+}
---
+++ Kitty-admin/src/test/java/fun/miaomi/Kittyadmin/KittyAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.miaomi.Kittyadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KittyAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
